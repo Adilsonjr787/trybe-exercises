598f934ca5af68bcc4bfc2b8179c1a094a0f1238
Adilsonjr787 <adilsonjr10@bol.com.br>
2022-04-22 16:50:08 -0300

    Exercise Pokedex

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,10 +1,18 @@
-
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import pokemons from './data';
+import Pokedex from './Components/Pokedex';
 import './App.css';
 
-function App() {
-  return (
-    
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Pokedex pokemons={pokemons} />
+      </div>
+    )
+  }
 }
 
 export default App;
