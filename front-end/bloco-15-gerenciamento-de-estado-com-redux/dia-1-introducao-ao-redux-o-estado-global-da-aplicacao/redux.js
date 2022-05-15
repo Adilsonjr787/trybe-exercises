2f1a4e08838d8cdd38b44b787d6fe70df339013f
Adilsonjr787 <adilsonjr10@bol.com.br>
2022-05-15 18:29:36 -0300

    Create Actions

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux.js
@@ -1,1 +1,18 @@
-const store = Redux.createStore(meuReducer);
+const reducer = (state = INICIAL_STATE, action) => {
+    switch (action.type) {
+      case 'NEXT_COLOR':
+        return {
+          ...state,
+          index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+        };
+      case 'PREVIOUS_COLOR':
+        return {
+          ...state,
+           index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+        };
+      default:
+        return state;
+    }
+  };
+
+const store = Redux.createStore(reducer);
